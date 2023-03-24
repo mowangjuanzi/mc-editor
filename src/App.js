f61bb55a69440629875822ffedd64c7d134630e9
mowangjuanzi <baoguoxiao0538@hotmail.com>
2023-03-24 18:08:00 +0800

    实现了基础的 p 标签

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import {createEditor} from "slate";
 import {Editable, Slate, withReact} from "slate-react";
 import './App.css';
@@ -10,14 +10,35 @@ const initValue = [
     }
 ];
 
+/**
+ * 默认元素
+ */
+const DefaultElement = props => {
+    return <p {...props.attributes}>{props.children}</p>
+}
+
+/**
+ * 打印 json 结构
+ */
+const SaveContent = value => {
+    console.log(JSON.stringify(value));
+}
+
 function App() {
 
     // 实例化编辑器
     const [editor] = useState(() => withReact(createEditor()))
 
+    // 渲染元素
+    const renderElement = useCallback(props => {
+        return <DefaultElement {...props} />
+    }, [])
+
     return (
-        <Slate editor={editor} value={initValue}>
-            <Editable />
+        <Slate editor={editor} value={initValue} onChange={SaveContent}>
+            <Editable
+                renderElement={renderElement}
+            />
         </Slate>
     );
 }
